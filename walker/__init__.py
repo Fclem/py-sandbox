a9a7b78baed850132a502a238a79625d4c514f7d
Clément Fiere <clement.fiere@helsinki.fi>
2017-10-18 01:51:30 +0300

    walker

--- walker/__init__.py
+++ walker/__init__.py
@@ -1,5 +1,6 @@
 import os
 import six
+import types
 
 
 def nop():
@@ -18,16 +19,62 @@ class FilterableList(list):
 		return six.text_type(self.__repr__())
 
 
+class FSObject(object):
+	path = six.text_type()
+	name = six.text_type()
+	dir_name = six.text_type()
+	is_file = False
+	is_dir = False
+	
+	def __init__(self, path, *args, **kwargs):
+		self.path = path if path[-1] != os.path.sep else path[:-1] # removes eventual trailing /
+		self.name = os.path.basename(path)
+		self.dir_name = os.path.dirname(path)
+		
+	def __repr__(self):
+		char = u'F' if self.is_file else u'D'
+		return six.text_type('%s<%s>' % (char, self.name))
+	
+	def __str__(self):
+		return six.text_type(self.path)
+
+
+class FileObject(FSObject):
+	is_file = True
+	is_dir = False
+	
+	def get_file_handle(self, mode='r', buffering=None):
+		return open(self.path, mode, buffering) if buffering is not None else open(self.path, mode)
+
+
+class DirObject(FSObject):
+	is_file = False
+	is_dir = True
+	pass
+		
+
 class WalkObject(object):
 	path = ''
 	dir_list = FilterableList
 	file_list = FilterableList
-	
+
 	def __init__(self, walk_object):
+		"""
+		
+		:param walk_object: (path, dir_list, file_list)
+		:type walk_object: (str, list, list)
+		"""
 		assert isinstance(walk_object, tuple) and len(walk_object) == 3
 		self.path = walk_object[0]
+		# self.dir_list = FilterableList([self.new_path(x) for x in walk_object[1]])
 		self.dir_list = FilterableList(walk_object[1])
 		self.file_list = FilterableList(walk_object[2])
+	
+	# def convert(self, a_list):
+	# 	return apply(lambda x: self.new_path(x), a_list)
+	
+	def new_path(self, a_name):
+		return '%s%s%s' % (self.path, os.path.sep, a_name)
 	
 	def filter_files(self, filter_func):
 		self.file_list = self.file_list.filter_func(filter_func)
@@ -44,7 +91,7 @@ class WalkObject(object):
 
 
 class ConfigGenerator(object):
-	path = str()
+	path = six.text_type()
 	verbose = False
 	_walker_list = list()  # type: list[WalkObject, ]
 	_filter_ext = list()  # type: list[six.text_type, ]
@@ -53,15 +100,12 @@ class ConfigGenerator(object):
 	
 	def __init__(self, a_path, exclude=list(), recursive=True, verbose=False):
 		self.verbose = verbose
-		self.path = a_path
-		# self._filter_ext = filter_ext
+		self.path = a_path if a_path[-1] != os.path.sep else a_path[:-1]  # removes eventual trailing /
 		self._exclude = exclude
 		self.recursive = recursive
 	
-	# self.walker_list = walker(a_path, _filter_ext, _exclude, verbose=verbose)
-	
 	def __str__(self):
-		return str(self.walker_list)
+		return six.text_type(self.walker_list)
 	
 	@property
 	def walker_list(self):
@@ -75,13 +119,8 @@ class ConfigGenerator(object):
 		:return:
 		:rtype: list[WalkObject]
 		"""
-		
 		walking = [i for i in os.walk(self.path)]
 		walk_list = walking if self.recursive else filter(lambda w: w[0] == self.path, walking)
-		# result = list()
-		# for walk_item in [WalkObject(x) for x in walk_list]:
-		# 	# walk_item.filter_files(bool)
-		# 	result.append(walk_item)
 		return [WalkObject(x) for x in walk_list]
 	
 	# return result
@@ -89,12 +128,9 @@ class ConfigGenerator(object):
 	def gen(self):
 		for each1 in self.walker_list:
 			print('%s' % each1)
-			for each2 in each1.dir_list:
-				# ConfigGenerator(each2).gen()
-				print('\t%s' % each2)
-	
-	# for each2 in each1.file_list:
-	# s	print('\t%s' % each2)
+			# for each2 in each1.dir_list:
+				#  ConfigGenerator(each2).gen()
+				#  print('\t%s' % each2)
 
 
 ConfigGenerator('/home/clem/_tmp/').gen()
